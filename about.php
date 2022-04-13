9c9096a0796d2b31ba6b621a506710ea6d1b70d8
IngoForrerNuila <ingoproductions@gmail.com>
2022-04-13 16:00:46 +0200

    Uploading General Changes version 2.0

---
+++ about.php
@@ -0,0 +1,28 @@
+<body>
+    <main>
+        <?php include("header.php");?>
+
+        <h1 class="text-center">WORLD WIDE ANIMALS</h1>
+        <h5 class="text-center">This is a quiz created by "Ingo Pytheas Forrer Nuila", using 
+            different languages as PHP, mySQL and JavaScript. The problem is that this guy
+            is still learning and unable to make more complex sites, with time I am really sure
+            that "Ingo" it is going to be able to increase his knowledge and make more beautiful
+            things.
+
+        </h5>
+
+        <img id="ingo" class="responsive" src="/img/profi.jpg">
+
+        <div id="nav-con" class="text-center">
+        
+            <button type="button"  id="nav-btn" class="btn btn-primary"><a href="index.php">HOME</a></button>
+                        
+        </div>
+
+
+        <?php include("footer.php");?>
+    </main>
+
+</body>
+
+</html>
--- header.php
+++ header.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WORLD WIDE ANIMALS</title>
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="js/main.js"></script>
+</head>
+
 <header>
 
     <video autoplay muted loop id="myVideo">
@@ -37,4 +51,3 @@ $secondQuestion= $Preguntas[1];
 $thirdQuestion= $Preguntas[2];
 ?>
 
-<?php echo '<h1 class="mb-3 text-center color-texto">' . $firstQuestion['Text'] . '</h1>' ?>
