0802141b4ec21d3aba502ae716ad11c0f2cf0a63
IngoForrerNuila <ingoproductions@gmail.com>
2022-04-26 14:11:29 +0200

    result-page-improvement-ver2

--- question1.php
+++ question1.php
@@ -1,9 +1,14 @@
+<?php
+session_start();
+$_SESSION["score"] = 0;
+?>
+
 <body>
     <main>
         <?php include("header.php");
-        /*print'<pre>';
-        print_r($Preguntas);
-        print'</pre>';*/
+        print'<pre>';
+        print_r($answerOne);
+        print'</pre>';
 
         ?>
 
@@ -11,34 +16,36 @@
 
         <?php echo '<h1 class="mb-3 text-center">' . $firstQuestion['Text'] . '</h1>' ?>
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="1" />
-            <label for="rd1"><?php echo '<h3 class="mb-3 text-center">' . $answerOne['Text'] . '</h3>' ?>
-            </label>
-        </div>
+   
+        
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="1" />
+                <label for="rd1"><?php echo '<h3 class="mb-3 text-center">' . $answerOne['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="0" />
-            <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerTwo['Text'] . '</h3>' ?>
-            </label>
-        </div>
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="0" />
+                <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerTwo['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="0" />
-            <label for="rd1">  <?php echo '<h3 class="mb-3 text-center">' . $answerThree['Text'] . '</h3>' ?>
-            </label>
-        </div>
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="0" />
+                <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerThree['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
+
+
+            <img id="global-pages-img" class="responsive" src="/img/camels.jpg">
+
+            <div id="nav-con" class="text-center">
+
+                <button type="submit" id="nav-btn" class="btn btn-primary"><a href="question2.php" class="text-decoration-none">ANSWER</a></button>
+
+            </div>
        
-
-        <img id="global-pages-img" class="responsive" src="/img/camels.jpg">
-
-        <div id="nav-con" class="text-center">
-
-            <button type="button" id="nav-btn" class="btn btn-primary"><a href="question2.php" class="text-decoration-none">ANSWER</a></button>
-
-        </div>
-
 
         <?php include("footer.php"); ?>
     </main>
--- question3.php
+++ question3.php
@@ -1,51 +1,62 @@
+<?php
+session_start();
+
+/* PLEASE CHANGE 3 WITH THE NUMBER OF THE CORRECT ANSWER OF THE QUESTION BEFORE*/
+if ($_POST["answer"] == 1) {
+    $_SESSION["score"] += 1;
+}
+?>
+
 <body>
     <main>
-        <?php include("header.php");?>
+        <?php include("header.php"); ?>
 
         <h1 class="text-center">QUESTION 3</h1>
-        
+
         <?php echo '<h1 class="mb-3 text-center">' . $thirdQuestion['Text'] . '</h1>' ?>
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="1" />
-            <label for="rd1"><?php echo '<h3 class="mb-3 text-center">' . $answerSix['Text'] . '</h3>' ?>
-            </label>
-        </div>
+        <form class="form" action="result.php" method="post">
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="0" />
-            <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerSeven['Text'] . '</h3>' ?>
-            </label>
-        </div>
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="1" />
+                <label for="rd1"><?php echo '<h3 class="mb-3 text-center">' . $answerSix['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="1" />
-            <label for="rd1">  <?php echo '<h3 class="mb-3 text-center">' . $answerEight['Text'] . '</h3>' ?>
-            </label>
-        </div>
-        
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="1" />
-            <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerNine['Text'] . '</h3>' ?>
-            </label>
-        </div>
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="0" />
+                <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerSeven['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
-        <div class="row justify-content-md-center text-center">
-            <input type="radio" name="user_level" id="rd1" value="1" />
-            <label for="rd1">  <?php echo '<h3 class="mb-3 text-center">' . $answerTen['Text'] . '</h3>' ?>
-            </label>
-        </div>
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="1" />
+                <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerEight['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
-        <img id="global-pages-img" class="responsive" src="/img/cow-mammals.jpg">
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="1" />
+                <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerNine['Text'] . '</h3>' ?>
+                </label>
+            </div>
 
-        <div id="nav-con" class="text-center">
-        
-            <button type="button"  id="nav-btn" class="btn btn-primary"><a href="result.php" class="text-decoration-none">ANSWER</a></button>
-                        
-        </div>
+            <div class="row justify-content-md-center text-center">
+                <input type="radio" name="user_level" id="rd1" value="1" />
+                <label for="rd1"> <?php echo '<h3 class="mb-3 text-center">' . $answerTen['Text'] . '</h3>' ?>
+                </label>
+            </div>
+
+            <img id="global-pages-img" class="responsive" src="/img/cow-mammals.jpg">
+
+            <div id="nav-con" class="text-center">
+
+                <button type="submit" id="nav-btn" class="btn btn-primary"><a href="result.php" class="text-decoration-none">ANSWER</a></button>
+
+            </div>
 
 
-        <?php include("footer.php");?>
+            <?php include("footer.php"); ?>
     </main>
 
 
